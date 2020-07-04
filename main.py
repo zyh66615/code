3d1ab82573729644638d3c4c7e87fefc0964b3e9
zyh66615 <zyh66615@github.com>
2020-07-04 23:18:22 +0800

    first

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+import time
+
+
+def task(self, name: str, age: int):
+    pass
+
+
+if __name__ == '__main__':
+    start = time.time()
+    print("程序启动！")
+    print("运行时间：", time.time()-start)
