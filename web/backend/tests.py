4c9c812f93e8dfe34e3f7da767ee1807f2cd1a7d
zyh66615 <zyh66615@github.com>
2020-07-12 22:38:00 +0800

    2020-07-12_22:38

--- web/backend/tests.py
+++ web/backend/tests.py
@@ -2,7 +2,7 @@
 @Description: 测试和杂项
 @Author: zyh
 @Date: 2020-07-09 10:34:27
-@LastEditTime: 2020-07-12 22:34:57
+@LastEditTime: 2020-07-12 22:36:17
 @LastEditors: zyh
 @FilePath: /web/backend/tests.py
 '''
@@ -97,7 +97,9 @@ def get_cookie(username, password):
 if __name__ == '__main__':
     start = time.time()
     cookie = get_cookie('13728902077', 'z123123123')
-    driver = webdriver.Firefox()
+    options = webdriver.ChromeOptions()
+    options.binary_location = '/home/zyh/code/web/chromedriver'
+    driver = webdriver.Chrome(chrome_options=options)
     driver.get('https://www.baidu.com')
     # driver.add_cookie(cookie)
     # driver.get('https://weibo.com/p/1008082c2fa1b7274dc344e5a228ba0983f864/super_index')
