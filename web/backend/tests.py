3fff44976a4e7738a8267ea219ce39b002fbad28
zyh66615 <zyh66615@github.com>
2020-07-13 13:45:00 +0800

    2020-07-13_13:45

--- web/backend/tests.py
+++ web/backend/tests.py
@@ -2,7 +2,7 @@
 @Description: 测试和杂项
 @Author: zyh
 @Date: 2020-07-09 10:34:27
-@LastEditTime: 2020-07-13 13:10:23
+@LastEditTime: 2020-07-13 13:42:34
 @LastEditors: zyh
 @FilePath: /web/backend/tests.py
 '''
@@ -97,24 +97,11 @@ def get_cookie(username, password):
     return cookies
 
 
-headers = {
-    'cookie': '_s_tentry=passport.weibo.com; Apache=7915648755954.881.1594565760526; SINAGLOBAL=7915648755954.881.1594565760526; ULV=1594565760570:1:1:1:7915648755954.881.1594565760526:; YF-V5-G0=7a7738669dbd9095bf06898e71d6256d; Ugrow-G0=5c7144e56a57a456abed1d1511ad79e8; WBtopGlobal_register_version=b35d30cc7b3b1f96; un=13728902077; login_sid_t=47fb5690c58c1170abe6707a51271f4e; cross_origin_proto=SSL; UOR=,,login.sina.com.cn; SSOLoginState=1594568602; SCF=AumcqJiIi3i4xJ7kR8jFqYySZvbmuFQXsyKe9BEJW3FyYSqjiZd9cD1xydraCxXmMlmkXmSJy3ZUck7PPqh97n8.; SUB=_2A25yD0FODeRhGeFN4lQY8SfJyziIHXVRfTWGrDV8PUJbmtAfLVLNkW9NQ6T1oARLHMbAM8VBUqtOacR98L18Zfb2; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WF.-6Ia5TxCNzXJUmM95JRV5JpX5K-hUgL.FoM01Kq4eK.fehB2dJLoI7_39PS0IgSyPNULqntt; SUHB=0sqxyO3peIH6fM; ALF=1626104973; wb_view_log_7396919504=2048*11521.25; YF-Page-G0=02467fca7cf40a590c28b8459d93fb95|1594570049|1594570049; webim_unReadCount=%7B%22time%22%3A1594570069702%2C%22dm_pub_total%22%3A0%2C%22chat_group_client%22%3A0%2C%22chat_group_notice%22%3A0%2C%22allcountNum%22%3A0%2C%22msgbox%22%3A0%7D',
-    'referer': 'https://weibo.com/p/1008082c2fa1b7274dc344e5a228ba0983f864/super_index',
-    'authority': 'weibo.com'
-}
+
 
 if __name__ == '__main__':
     start = time.time()
-    username = '13728902077'
-    password = 'z123123123'
-    # session = login(username, password)
-    # response = requests.get('https://weibo.com/p/aj/general/button?ajwvr=6&api=http://i.huati.weibo.com/aj/super/checkin&texta=%E7%AD%BE%E5%88%B0&textb=%E5%B7%B2%E7%AD%BE%E5%88%B0&status=0&id=1008082c2fa1b7274dc344e5a228ba0983f864&location=page_100808_super_index&timezone=GMT+0800&lang=zh-cn&plat=Win32&ua=Mozilla/5.0%20(Windows%20NT%2010.0;%20Win64;%20x64)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/83.0.4103.116%20Safari/537.36%20Edg/83.0.478.61&screen=2048*1152', headers=headers)
-    # jsonstr = response.content.decode('gbk')
-    # info = json.loads(jsonstr)
-    # print(info)
-    a = {'1': 'www', '2': 'rrr'}
-    for i, j in a:
-        print(i, j)
+    
     # print(response.status_code)
     # driver = webdriver.PhantomJS(
     #     './phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
