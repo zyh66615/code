2dbad77032b278fae87ba7823d496133757ed2d6
zyh66615 <zyh66615@github.com>
2020-07-17 22:25:46 +0800

    2020-07-17_22:25

---
+++ web/main.go
@@ -0,0 +1,19 @@
+/*
+ * @Description: 第一次学习golang语法
+ * @Author: zyh
+ * @Date: 2020-07-17 21:02:06
+ * @LastEditTime: 2020-07-17 22:25:27
+ * @LastEditors: zyh
+ * @FilePath: /web/main.go
+ */ 
+package main
+
+import "fmt"
+
+func main()  {
+	//第一行
+	var a string
+	fmt.Scanln(&a)
+	fmt.Println("Hello World" + a)
+	
+}
