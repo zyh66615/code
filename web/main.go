3d34ea9e9d8ff5fe327356f9a4fd35273c4655ec
zyh66615 <zyh66615@github.com>
2020-07-18 10:00:40 +0800

    2020-07-18_10:00

--- web/main.go
+++ web/main.go
@@ -2,18 +2,26 @@
  * @Description: 第一次学习golang语法
  * @Author: zyh
  * @Date: 2020-07-17 21:02:06
- * @LastEditTime: 2020-07-17 22:25:27
+ * @LastEditTime: 2020-07-17 22:56:06
  * @LastEditors: zyh
  * @FilePath: /web/main.go
  */ 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+	"math"
+)
 
 func main()  {
 	//第一行
-	var a string
-	fmt.Scanln(&a)
-	fmt.Println("Hello World" + a)
+	t1:=time.Now()
+	sum:=0
+	for i:=0;i<int(math.Pow10(8));i++ {
+		sum++
+	}
+	end:= time.Since(t1)
+	fmt.Println(end)
 	
 }
