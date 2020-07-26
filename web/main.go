4708864f24dd25e826ef55819d1a288f92d8fbaf
张宇华69912 <69912@sangfor.com>
2020-07-26 09:03:07 +0800

    2020-07-26_09:03

--- web/main.go
+++ web/main.go
@@ -2,26 +2,37 @@
  * @Description: 第一次学习golang语法
  * @Author: zyh
  * @Date: 2020-07-17 21:02:06
- * @LastEditTime: 2020-07-18 10:16:36
+ * @LastEditTime: 2020-07-25 20:28:29
  * @LastEditors: zyh
  * @FilePath: /web/main.go
- */ 
+ */
 package main
 
 import (
 	"fmt"
+	"reflect"
+	"strconv"
 	"time"
-	"math"
 )
 
-func main()  {
+func main() {
 	//第一行
-	t1:=time.Now()
-	sum:=0
-	for i:=0;i<int(math.Pow10(8));i++ {
-		sum++
+	t1 := time.Now()
+	s := test(5, 10)
+	fmt.Println(reflect.TypeOf(s))
+	for i := 0; i < len(s); i++ {
+		fmt.Println(s[i])
 	}
-	end:= time.Since(t1)
+	end := time.Since(t1)
 	fmt.Println(end)
-	
+
+}
+func test(n1, n2 int) []string {
+	res := make([]string, n1)
+	for i := 0; i < n1; i++ {
+		for j := 0; j < n2; j++ {
+			res[i] += strconv.Itoa(j+1)
+		}
+	}
+	return res
 }
